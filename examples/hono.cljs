45eff723f9a4ecb531ad258bef62e50cded6cd76
Michiel Borkent <michielborkent@gmail.com>
2026-08-09 12:08:00 +0200

    Add hono, markdown server and preact ssr examples

---
+++ examples/hono.cljs
@@ -0,0 +1,12 @@
+;; web server with hono from esm.sh
+;; run with: cherry-quickjs examples/hono.cljs
+(require '["https://esm.sh/hono" :refer [Hono]]
+         '[cherry.http :refer [serve]])
+
+(def app (Hono.))
+
+(.get app "/" (fn [c] (.text c "hello from hono")))
+(.get app "/json" (fn [c] (.json c #js {:framework "hono" :runtime "cherry-quickjs"})))
+(.post app "/echo" (fn [c] (.then (.text (.-req c)) (fn [b] (.text c b)))))
+
+(serve (.-fetch app) {:port 3000})
---
+++ examples/ssr.cljs
@@ -0,0 +1,16 @@
+;; server-side rendering with preact from esm.sh
+;; run with: cherry-quickjs examples/ssr.cljs
+(require '["https://esm.sh/preact-render-to-string@6.5.11?deps=preact@10.24.3" :refer [render]]
+         '["https://esm.sh/preact@10.24.3" :refer [h]])
+
+(defn todo-list [items]
+  (h "ul" #js {:class "todos"}
+     (into-array (map #(h "li" nil %) items))))
+
+(defn page [items]
+  (h "html" nil
+     (into-array
+      [(h "head" nil (h "title" nil "todos"))
+       (h "body" nil (todo-list items))])))
+
+(println (render (page ["write examples" "push to github"])))
